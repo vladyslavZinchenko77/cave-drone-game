c8bdede61859616739d68b2941f877c6bca4fff3
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-03-19 11:24:10 +0200

    added pages

---
+++ src/pages/GamePage/GamePage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const GamePage: FC = () => {
+  return <div>GamePage</div>;
+};
+
+export default GamePage;
---
+++ src/pages/ScoreBoardPage/ScoreBoardPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const ScoreBoardPage: FC = () => {
+  return <div>ScoreBoardPage</div>;
+};
+
+export default ScoreBoardPage;
