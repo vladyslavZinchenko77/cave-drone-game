870735930de909e1434b3b7704922852eb888ebc
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-03-19 19:53:05 +0200

    made a structure of gamepage

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,7 +1,17 @@
 import { FC } from 'react';
+import Cave from '../../components/Cave/Cave';
+import Drone from '../../components/Drone/Drone';
+import GameControl from '../../components/GameControl/GameControl';
 
 const GamePage: FC = () => {
-  return <div>GamePage</div>;
+  return (
+    <div>
+      <h2>Game is starting</h2>
+      <Cave />
+      <Drone />
+      <GameControl />
+    </div>
+  );
 };
 
 export default GamePage;
